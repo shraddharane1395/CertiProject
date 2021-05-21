7cbf6cd8cf486297e7b2fcb6284f4951d625ae2a
shraddharane1395 <63951302+shraddharane1395@users.noreply.github.com>
2021-05-21 16:23:07 +0530

    Update home.php

--- website/content/home.php
+++ website/content/home.php
@@ -1,2 +1,2 @@
-<p>This is <b>home</b> Welcome to test project !!!!</p>
+<p>This is <b>home</b> Welcome to test project !!!!!</p>
 </p>You can modify here to verify whether CI/CD pipeline is working as expected or not .</p>
